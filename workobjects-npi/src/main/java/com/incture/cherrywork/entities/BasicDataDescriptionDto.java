4110855dd13c2e09b9db83778b684fbd08325451
rameshreddymalipatel <ramesh0067@gmail.com>
2021-02-19 11:48:21 +0530

    Generate COM Spring Boot Template

---
+++ workobjects-npi/src/main/java/com/incture/cherrywork/entities/BasicDataDescriptionDto.java
@@ -0,0 +1,20 @@
+package com.incture.cherrywork.dtos;
+public class BasicDataDescriptionDto{
+
+
+private String language;
+public String getLanguage(){
+	return language;
+}
+public void setLanguage(String language){
+	this.language = language;
+}
+
+private String description;
+public String getDescription(){
+	return description;
+}
+public void setDescription(String description){
+	this.description = description;
+}
+}
