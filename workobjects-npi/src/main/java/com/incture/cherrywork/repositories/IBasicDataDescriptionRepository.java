48d0a72919c0b3b9f68caebbf108eb7d493a5231
rameshreddymalipatel <ramesh0067@gmail.com>
2021-02-22 11:19:40 +0530

    2

--- workobjects-npi/src/main/java/com/incture/cherrywork/repositories/IBasicDataDescriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.incture.cherrywork.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.incture.cherrywork.entities.BasicDataDescription;
-
-public interface IBasicDataDescriptionRepository extends JpaRepository<BasicDataDescription, String>{
-    }
